dae7ae679e3e0ba90b23819c4ac3004cc0bf2aa0
prashant singh <prashantsinghmangat5@gmail.com>
2020-04-06 19:16:25 +0530

    Initial commit

---
+++ app/src/main/java/com/demotxt/myapp/myapplication/activities/MainActivity.java
@@ -0,0 +1,100 @@
+package com.demotxt.myapp.myapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.demotxt.myapp.myapplication.R;
+import com.demotxt.myapp.myapplication.adapters.RecyclerViewAdapter;
+import com.demotxt.myapp.myapplication.model.Anime;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String JSON_URL = "http://13.126.27.50/MHMS_DEV/rest/searchpatient/givenName/akshay" ;
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Anime> lstAnime ;
+    private RecyclerView recyclerView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstAnime = new ArrayList<>() ;
+        recyclerView = findViewById(R.id.recyclerviewid);
+        jsonrequest();
+
+
+
+    }
+
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Anime anime = new Anime() ;
+                        anime.setGivenName(jsonObject.getString("givenName"));
+                        anime.setMiddleName(jsonObject.getString("middleName"));
+                        anime.setEmail(jsonObject.getString("email"));
+                        anime.setPhoneNumber(jsonObject.getString("phoneNumber"));
+                        //anime.setDateOfBirth(jsonObject.getInt("dateOfBirth"));
+                        anime.setPersonId(jsonObject.getString("personId"));
+                        //anime.setImage_url(jsonObject.getString("img"));
+                        lstAnime.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+                setuprecyclerview(lstAnime);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request) ;
+
+
+    }
+
+    private void setuprecyclerview(List<Anime> lstAnime) {
+
+
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(this,lstAnime) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myadapter);
+
+    }
+}
---
+++ app/src/main/java/com/demotxt/myapp/myapplication/model/Anime.java
@@ -0,0 +1,87 @@
+package com.demotxt.myapp.myapplication.model;
+
+/**
+ * Created by Aws on 11/03/2018.
+ */
+
+public class Anime {
+
+    private String givenName ;
+    private String middleName;
+    private String email;
+    private int dateOfBirth;
+    private String phoneNumber;
+    private String personId ;
+    private String image_url;
+
+    public Anime() {
+    }
+
+    public Anime(String givenName, String middleName, String email, int dateOfBirth, String phoneNumber, String personId, String image_url) {
+        this.givenName = givenName;
+        this.middleName = middleName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.personId = personId;
+        this.image_url = image_url;
+    }
+
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
