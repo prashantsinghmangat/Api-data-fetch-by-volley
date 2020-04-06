c3e9f8b433939c1cea42c2002dc5d3a3723cf88a
prashant singh <prashantsinghmangat5@gmail.com>
2020-04-06 20:08:44 +0530

    commit api volley

---
+++ app/src/main/java/com/demotxt/myapp/myapplication/Login/LoginBmr.java
@@ -0,0 +1,104 @@
+package com.demotxt.myapp.myapplication.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.demotxt.myapp.myapplication.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+
+public class LoginBmr extends AppCompatActivity {
+
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private ImageView image;
+    private int counter = 5;
+    OkHttpClient client = new OkHttpClient();
+
+    /*private final String GlobalVariables = "http://13.126.27.50/MHMS_DEV/user/";
+    private final String GlobalVariablesRest = "http://13.126.27.50/MHMS_DEV/rest/";
+    private final String CORS = "http://13.126.27.50";*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_bmr);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("inside on click listener");
+
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        MHPFlow mhpFlow = new MHPFlow();
+                        String information = null;
+                        final String userName = ((EditText) findViewById(R.id.etName)).getText().toString();
+                        final String password = ((EditText) findViewById(R.id.etPassword)).getText().toString();
+                        System.out.println(userName+"     --------------------------     " +password);
+
+                        String jwtToken = mhpFlow.login(userName, password);
+
+
+                        try {
+                            Log.e("try","inside LoginBmr try----------------------------------");
+                            Log.e("JWT Token", jwtToken);
+                            JSONObject jsonObjectbject = new JSONObject(jwtToken);
+                            String token = jsonObjectbject.getString("token");
+                            Log.e("Token", token);
+                            JSONObject result = new JSONObject(MHPFlow.decoded(jwtToken));
+                            Log.e("decoded String (result)", result.toString());
+                            Log.e("getAssociatedOrg()",mhpFlow.getAssociatedOrg(token, result.getString("sessionToken")).toString());
+                            JSONArray jsonArray = mhpFlow.getAssociatedOrg(token, result.getString("sessionToken"));
+                            //System.out.println(jsonArray.getJSONObject(jsonArray.length()-1));
+                            int cnt = 0;
+                            ArrayList<String> list = new ArrayList<String>();
+                            list.add("MHE/OP*");
+                            while(cnt < jsonArray.length()){
+                                JSONObject mheObject = jsonArray.getJSONObject(cnt);
+                                list.add(mheObject.getString("name"));
+                                cnt++;
+                            }
+                            Log.e("MHE list", list.toString());
+                            Log.e("try", "outside LoginBmr try----------------------------------");
+                            Intent intent = new Intent(LoginBmr.this, SelectMhe.class);
+                            intent.putExtra("list", list);
+                            LoginBmr.this.startActivity(intent);
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                };
+                new Thread(runnable).start();
+            }
+        });
+    }
+
+} //Login ends
---
+++ app/src/main/java/com/demotxt/myapp/myapplication/Login/SelectMhe.java
@@ -0,0 +1,77 @@
+package com.demotxt.myapp.myapplication.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import com.demotxt.myapp.myapplication.Dashboardbmr.activities.MainActivity;
+import com.demotxt.myapp.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectMhe extends AppCompatActivity {
+    private Spinner spinnerMhe;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_mhe);
+
+        Intent intent = getIntent();
+
+        System.out.println("###################################################value = " + ((ArrayList<String>)intent.getSerializableExtra("list")).toString());
+        setContentView(R.layout.activity_select_mhe);
+
+        spinnerMhe = findViewById(R.id.spinnerMhe);
+
+        List<String> categories = (ArrayList<String>)intent.getSerializableExtra("list");
+        /*categories.add(0,"MHE/OP*");
+        categories.add("Ted");
+        categories.add("Kartik clinic");
+        categories.add("metro");
+        categories.add("SRM");*/
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinnerMhe.setAdapter(dataAdapter);
+
+        spinnerMhe.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(parent.getItemAtPosition(position).equals("MHE/OP*"))
+                {
+                    //do nothing
+                }
+                else
+                {
+                    //on selecting spinner item
+                    String item = parent.getItemAtPosition(position).toString();
+
+                    //show spinner selected item
+                    Toast.makeText(parent.getContext(), "Selected" + item, Toast.LENGTH_SHORT).show();
+                    if(parent.getItemAtPosition(position).equals("metro"));
+                    {
+                        Intent intent = new Intent(SelectMhe.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
